e419b29786a5a8bf673d1671d567f2696ef99ac1
petersilvahs <petersilvahs@gmail.com>
2023-06-20 20:12:33 -0300

    feat: add header component

--- src/AppRouters.tsx
+++ src/AppRouters.tsx
@@ -3,12 +3,13 @@ import {
 } from "react-router-dom"
 import { Plans } from "./pages/Plans"
 import { Home } from "./pages/Home"
+import Header from "./components/header"
 
 export function AppRouters() {
    return (
       <Router>
          <Routes>
-            <Route path="/card" element={<Plans />} />
+            <Route path="/card" element={<Header />} />
             <Route path="/catalogo" element={<Home />} />
          </Routes>
       </Router>
