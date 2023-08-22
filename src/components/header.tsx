8400a5884b0b0e2eb1247a8ad234f8cf1afedebc
petersilvahs <petersilvahs@gmail.com>
2023-08-21 22:56:23 -0300

    chore: move style at header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,14 +5,17 @@ import { MudeLogo } from "../images/mude-logo"
 export function Header(){
    return(
       <Flex justifyContent="space-between">
-         <Flex height="10rem" width="10rem" marginTop="-3rem">
+         <Flex height="10rem" width="10rem" marginTop="-3rem" marginLeft="3rem">
          <MudeLogo />
          </Flex>
+         <Flex gap="5rem">
          <Text color="#110229">INICIO</Text>
          <Text color="#110229">SERVIÇOS</Text>
          <Text color="#110229">PLANOS</Text>
          <Text color="#110229">CIDADES</Text>
-         <Button color="#1d267a">LOGIN</Button>
+         </Flex>
+         {/* button problem */}
+         <Button backgroundColor="#1d267a" size="md" borderRadius="2rem" variant="outline" marginRight="3rem">LOGIN</Button>
       </Flex>
    )
 }
